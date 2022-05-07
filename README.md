729fe574720b8212276e296a3bd0296fc19facde
Andrey Naryshkin <andrej.naryshkin@gmail.com>
2022-05-07 14:13:02 +0300

    add HTTP v1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # FCM Mock Server GO (docker)
 
-Simple FCM mock for testing server applications.
+Simple FCM mock for both old fcm HTTP and HTTP v1
 Image for this repo on [DockerHub](https://hub.docker.com/repository/docker/andruxxxa/fcm-mock-server-go)
 
 ## Quick start
@@ -19,14 +19,16 @@ go run main.go
 ## API
 
 Endpoints:
-* **POST** */send* - send message to the mock (200 response)
-* **GET** */messages* - returns (as `JSON`) history of `FCM` API calls to this mock
-* **DELETE** */messages* - clear messages history
+* **POST** */send* - send message to the mock (200 response) - old HTTP
+* **POST** */v1/projects/[project_id]/messages:send* - send message to the mock (200 response) - HTTP V1
+* **GET** */api/messages* - returns (as `JSON`) history of `FCM` API calls to this mock
+* **DELETE** */api/messages* - clear messages history
 
 ## Message model
+### Old HTTP
 ```js
 {
-	"to": "123",    // device token or topic
+    "to": "string",    // device token or topic
     "data": {       // [string]string dictionary
         "field1": "value1",
 	    "field2": "value2",
@@ -38,7 +40,25 @@ Endpoints:
     }
 }
 ```
-
+### HTTP V1
+```js
+{
+    "Message": {
+        "token": "string",    
+        "topic" : "string",
+        "condition" : "string",
+        "data": {       // [string]string dictionary
+            "field1": "value1",
+            "field2": "value2",
+            ...
+        },
+        "notification": {
+            "title": "string",
+            "body": "string"
+        }
+    }
+}
+```
 ## test.http
 
 Simple test requests - work with [REST client](https://marketplace.visualstudio.com/items?itemName=humao.rest-client)
--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+type MessageV1 struct {
+	Message Message `json:"Message"`
+}
+
 type Message struct {
-	To           string            `json:"to"`
-	Notification Notification      `json:"notification"`
-	Data         map[string]string `json:"data"`
-	Time         string            `json:"time"`
+	Authorization string            `json:"authorization,omitempty"`
+	To            string            `json:"to,omitempty"`
+	Token         string            `json:"token,omitempty"`
+	Topic         string            `json:"topic,omitempty"`
+	Condition     string            `json:"condition,omitempty"`
+	Project       string            `json:"project,omitempty"`
+	Notification  Notification      `json:"notification"`
+	Data          map[string]string `json:"data,omitempty"`
+	Time          string            `json:"time"`
 }
 
 type Notification struct {
@@ -24,15 +35,16 @@ func main() {
 	app := fiber.New()
 
 	app.Post("/send", send)
-	app.Delete("/messages", clear)
-	app.Get("/messages", log)
+	app.Post("/v1/projects/:project_id/messages\\:send", sendV1)
+	app.Delete("/api/messages", clear)
+	app.Get("/api/messages", log)
 
 	app.Listen(":4004")
 }
 
 func send(c *fiber.Ctx) error {
 	auth := c.Get("Authorization")
-	if len(auth) == 0 {
+	if len(auth) == 0 || !strings.HasPrefix(auth, "key=") {
 		return c.SendStatus(401)
 	}
 	message := new(Message)
@@ -45,7 +57,28 @@ func send(c *fiber.Ctx) error {
 	return c.SendStatus(200)
 }
 
+func sendV1(c *fiber.Ctx) error {
+	auth := c.Get("Authorization")
+	if len(auth) == 0 || !strings.HasPrefix(auth, "Bearer") {
+		return c.SendStatus(401)
+	}
+	messageV1 := new(MessageV1)
+	if err := c.BodyParser(messageV1); err != nil {
+		return c.Status(400).SendString(err.Error())
+	}
+	message := messageV1.Message
+	currentTime := time.Now()
+	message.Time = currentTime.Format("2006-01-02 15:04:05")
+	message.Project = c.Params("project_id")
+	messages = append(messages, message)
+	return c.SendStatus(200)
+}
+
 func log(c *fiber.Ctx) error {
+	sort.SliceStable(messages, func(i, j int) bool {
+		return messages[i].Time > messages[j].Time
+	})
+
 	return c.JSON(messages)
 }
 
